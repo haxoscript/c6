d7605ea374a3aa216e78028b7141e5826208b8cc
haxoscript <43457466+haxoscript@users.noreply.github.com>
2018-10-04 10:46:42 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = 
+"c3ae3ad6-a82a-4fc4-ad6b-df439a9692db";
+
+// kamu bisa membuat banyak config untuk beberapa akun.
